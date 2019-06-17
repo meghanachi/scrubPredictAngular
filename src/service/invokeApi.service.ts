b01bbde508257d5ebd6a6ac40cb97ecb06c8c6a2
meghanachi <51138584+meghanachi@users.noreply.github.com>
2019-06-16 19:41:07 -0700

    Update invokeApi.service.ts

--- src/service/invokeApi.service.ts
+++ src/service/invokeApi.service.ts
@@ -5,6 +5,10 @@ import { HttpHandler, ɵHttpInterceptingHandler } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import {RootModal} from '../app/models/rootModal';
+import {Inputs} from '../app/models/Inputs';
+import { Input } from '@angular/compiler/src/core';
+
+
 
 
 @Injectable({
@@ -14,11 +18,13 @@ export class scrubService {
     apiUrl = 'https://ussouthcentral.services.azureml.net/workspaces/86a4021753874ee68e6ef949890b5533/services/01efe0bdf3d44a7b837c051dbcb755f5/execute?api-version=2.0&details=true';
    apiKey = "75lYrn06B2y3TdM0yeX6CQgmVVXnRmagtEIraZ4BhZNoxUr4E3DlHjGQiTFFAASjCSGqDZsgOaQhGAgoE1x5KQ==";  // URL to web api
 
+
+
   constructor(
     private http: HttpClient) {
   }
 
-calltheAPI(request: RootModal): Observable<any> {
+calltheAPI(request: Inputs): Observable<any> {
 
   const injector = Injector.create({
     providers: [
@@ -28,17 +34,19 @@ calltheAPI(request: RootModal): Observable<any> {
 });
 const httpClient: HttpClient = injector.get(HttpClient);
 
-    const headers = new HttpHeaders;
-    headers.set('Content-Type', 'application/json');
-    headers.set('Authorization', 'Bearer' + this.apiKey)
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'Bearer' + ' ' +  this.apiKey
+  })
+};
+console.log(httpOptions.headers.get("Authorization"));
 
-    headers.set('Access-Control-Allow-Origin', 'http://localhost:4200');
-headers.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
     let httpHandlers: HttpHandler;
     this.http = new HttpClient(httpHandlers);
     //return this.http.request(new HttpRequest("POST", this.apiUrl, {headers: headers}));
-    return httpClient.post(this.apiUrl, request,  { headers: headers })
+    return httpClient.post(this.apiUrl, request,  httpOptions)
   }
 
 }
