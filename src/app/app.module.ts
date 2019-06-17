d59218659b8b4fe02fba91059e85d2c289e28cf8
meghanachi <51138584+meghanachi@users.noreply.github.com>
2019-06-16 19:48:14 -0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule,  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {scrubService} from '../service/invokeApi.service';
